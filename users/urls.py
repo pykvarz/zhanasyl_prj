cfce3028129a831f0d43e499e84efbd3b0194986
pykvarz <ibraev.ias@gmail.com>
2023-11-23 17:49:21 +0600

    Стабильно

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import CustomUserCreateView, CustomUserLoginView, DashboardView, AddPermView
+from users.views import CustomUserCreateView, CustomUserLoginView, DashboardView, AddPermView, TestView
 
 urlpatterns = [
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', CustomUserCreateView.as_view(), name='register'),
     path('login/', CustomUserLoginView.as_view(), name='login'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('check/', AddPermView.as_view(), name='check'),
+    path('test/', TestView.as_view(), name='test'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import permission_required, login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, FormView
 
 from users.forms import CustomUserCreationForm, CheckKeyForm
 from users.models import CustomUser
@@ -22,42 +23,43 @@ class CustomUserCreateView(CreateView):
     model = CustomUser
     form_class = CustomUserCreationForm
     template_name = 'register.html'
-    success_url = 'user/login/'  # Замените '/success/' на ваш URL успешного создания объекта
+    success_url = '/user/login/'
 
-
-class CustomUserLoginView(LoginRequiredMixin, LoginView):
-    template_name = 'login.html'
-    login_url = '/user/dashboard/'
-
-    def get(self, request, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return HttpResponseRedirect('/user/dashboard')
+            return redirect("dashboard")
+        return super().dispatch(request, *args, **kwargs)
 
 
+class CustomUserLoginView(LoginView):
+    redirect_authenticated_user = True
+    template_name = 'login.html'
 
 
-@method_decorator(permission_required('users.view_dashboard'), name='dispatch')
+@method_decorator(permission_required('users.view_dashboard', login_url="/user/check"), name='dispatch')
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
 
 
-class AddPermView(LoginRequiredMixin, View):
+class AddPermView(LoginRequiredMixin, UserPassesTestMixin, FormView):
     template_name = 'check_code.html'
+    form_class = CheckKeyForm
+    success_url = reverse_lazy('dashboard')
 
-    def get(self, request):
-        form = CheckKeyForm()
-        return render(request, self.template_name, {'form': form})
+    def test_func(self):
+        return not self.request.user.user_permissions.filter(pk=32).exists()
 
-    def post(self, request):
-        form = CheckKeyForm(request.POST)
-        if form.is_valid():
-            key = form.cleaned_data['key']
-            # result = check_key(key="534d66eeaff3edd6e0b1008139dac451e265c5390eb10db5a4c9311fef2d8466")
-            # logger.info(result)
-            # return HttpResponse(result)
-            if check_key(key=key):
-                add_perm(user_id=request.user.id, p_id=32)
-                return HttpResponseRedirect('/user/dashboard/')
-            else:
-                return render(request, self.template_name, {"form": form})
-        return render(request, self.template_name, {"form": form})
+    def handle_no_permission(self):
+        return HttpResponseRedirect(self.success_url)
+
+    def form_valid(self, form):
+        key = form.cleaned_data['key']
+        if check_key(key=key):
+            add_perm(user_id=self.request.user.id, p_id=32)
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+
+class TestView(TemplateView):
+    template_name = 'test.html'
